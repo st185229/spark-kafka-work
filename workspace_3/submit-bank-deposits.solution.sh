fa6bb3f4429cf07b01528b2513fa65d59ed60cd6
st185229 <suresh.thomas.uk@gmail.com>
2021-06-02 19:19:41 +0100

    Added bank deppsit - views

---
+++ workspace_3/submit-bank-deposits.solution.sh
@@ -0,0 +1,1 @@
+/home/workspace/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.6 /home/workspace/bank-deposits.solution.py | tee /home/workspace/spark/logs/bank-deposits-solution.log 
