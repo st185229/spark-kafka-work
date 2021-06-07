ee507378c628730ccd556368d96c9921eb53e5ef
st185229 <suresh.thomas.uk@gmail.com>
2021-06-07 08:54:37 +0100

    spark and radius

---
+++ workspace_4/submit-customer-location.sh
@@ -0,0 +1,3 @@
+
+/home/workspace/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.6 /home/workspace/customer-location.py | tee /home/workspace/spark/logs/customer-location.log
+​
